84087f30a64b47b7c23a760653a1540d998033ca
wanp <wanpan96@163.com>
2021-04-18 18:15:23 +0800

    update

--- src/pages/content/page_1/index.jsx
+++ src/pages/content/page_1/index.jsx
@@ -1,25 +1,25 @@
 import React, { Component, Fragment } from 'react'
-import CSSTransition from 'react-addons-css-transition-group'
+import CSSTransitionGroup from 'react-transition-group';
+
+import './index.scss'
 
 export default class page_1 extends Component {
 
     state = {
-        show: true
+        isShow: true
     }
 
     render() {
         return (
             <Fragment>
-                <CSSTransition
-                    in={this.state.show}
-                    timeout={1000}
-                    classNames='fade'
-                    unmountOnExit
-                    onEntered={el => { el.style.color = 'blue' }}
+                <CSSTransitionGroup
+                    in={this.state.isShow}
+                    timeout={2000}
+                    classNames="test"
                     appear={true}
                 >
                     <div>hello</div>
-                </CSSTransition>
+                </CSSTransitionGroup>
                 <button onClick={this.handleToggole}>toggole</button>
             </Fragment>
         )
@@ -27,7 +27,7 @@ export default class page_1 extends Component {
 
     handleToggole = () => {
         this.setState({
-            show: this.state.show ? false : true
+            show: this.state.isShow ? false : true
         })
     }
 
