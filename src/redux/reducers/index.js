15d0602ab06d43e3edfc4489dfed831635392d09
Wanp <wanpan96@163.com>
2021-07-06 17:37:14 +0800

    合并分支

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,4 +3,4 @@ import test_1 from './test_1'
 import test_2 from './test_2'
 
 /* 合并 store 后每次 dispatch 被调用时 都会调用所有 reducers  */
-export default combineReducers({ test_1, test_2 });
+export default combineReducers({ test_1, test_2 });/*  */
